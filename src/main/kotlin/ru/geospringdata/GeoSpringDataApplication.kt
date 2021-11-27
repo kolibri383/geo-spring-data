6803198604d71dd3db9a5af9d1bff281e55ba43e
dima.dima.16173@gmail.com <dima02062001>
2021-11-27 23:17:49 +0300

    Empty project

---
+++ src/main/kotlin/ru/geospringdata/GeoSpringDataApplication.kt
@@ -0,0 +1,11 @@
+package ru.geospringdata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeoSpringDataApplication
+
+fun main(args: Array<String>) {
+	runApplication<GeoSpringDataApplication>(*args)
+}
---
+++ src/test/kotlin/ru/geospringdata/GeoSpringDataApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.geospringdata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeoSpringDataApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
